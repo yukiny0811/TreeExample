939ae2940672778e16f96cc1bd84bd8e2e83b81a
yukiny0811 <yukiny0811@gmail.com>
2024-08-26 01:50:43 +0900

    Initial Commit

---
+++ TreeExample/TreeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeExampleApp.swift
+//  TreeExample
+//
+//  Created by Yuki Kuwashima on 2024/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TreeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
